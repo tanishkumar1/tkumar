a690f2db03f2f75d165dcb10f2a96f120f9270ff
tanishkumar1 <60348368+tanishkumar1@users.noreply.github.com>
2020-01-28 17:13:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "35.222.71.185";
+$username = "newuser";
+$dbpass = "Tanish@123";
+$dbname="dbs";
+
+  $email = $_POST["username"];
+  $pass=$_POST["pass"];
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $dbpass,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "INSERT INTO reg_1 (email,pass)
+VALUES ('$email','$pass')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
